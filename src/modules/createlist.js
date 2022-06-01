1a00896543d69dbba272325f1552d067cd6e2e0f
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-01 16:24:01 +0100

    create listItem elements

--- src/modules/createlist.js
+++ src/modules/createlist.js
@@ -1,6 +1,31 @@
 /* eslint-disable linebreak-style */
+const toDoList = document.querySelector('.to-do-list');
 
-const listItems = () => {
-  
+const listItems = ({description}) => {
+  const li = document.createElement('li');
+  toDoList.appendChild(li);
+  const div = document.createElement('div');
+  div.classList.add('list-container');
+  li.appendChild(div);
+
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.classList.add('check-list');
+  div.appendChild(checkbox);
+
+  const text = document.createElement('input');
+  text.setAttribute('type', 'text');
+  text.setAttribute('placeholder', description);
+  text.setAttribute('disabled', '');
+  div.appendChild(text);
+
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.innerHTML = `
+  <button type="buton">
+    <i class="fa fa-ellipsis-vertical"></i>
+  </button>`;
+  li.appendChild(button);
 };
+
 export default listItems;
