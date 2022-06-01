50554fe983bb2d1447e2647151507f36919c37f0
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-01 14:02:57 +0100

    Add HTML markup and style.css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import _ from 'lodash';
 import './style.css';
-
- function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-
-   return element;
- }
-
- document.body.appendChild(component());
