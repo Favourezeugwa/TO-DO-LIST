3b6f3b0ba1976fd40f648942bbb7d5854a84ce87
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-03 12:46:41 +0100

    Add Interactive list

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import listItems from './modules/createlist.js';
 import { list, SaveItem } from './modules/class.js';
 
 const form = document.querySelector('#add-form');
+const toDoList = document.querySelector('.to-do-list');
+const clearCompleted = document.querySelector('#clear-btn');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -20,3 +22,28 @@ for (let i = 1; i <= list.length; i += 1) {
     }
   });
 }
+
+// clear all completed fields
+clearCompleted.addEventListener('click', () => {
+  const newlist = list.filter((element) => element.completed === false);
+  list.forEach((element) => {
+    if (!newlist.includes(element)) {
+      list.splice(list.indexOf(element), 1);
+    }
+  });
+  let i = 1;
+  list.forEach((element) => {
+    element.index = i;
+    i += 1;
+  });
+  toDoList.innerHTML = '';
+  for (let i = 1; i <= list.length; i += 1) {
+    list.forEach((listItem) => {
+      if (listItem.index === i) {
+        listItems(listItem);
+      }
+    });
+  }
+  localStorage.setItem('list', JSON.stringify(list));
+});
+
